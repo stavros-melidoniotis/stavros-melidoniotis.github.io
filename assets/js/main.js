ff1c6665c96417512adac776c44ec2370ba7ee3e
stavros-melidoniotis <melidon.stavros@gmail.com>
2021-06-11 23:15:03 +0300

    Change visible section from tabs and explorer menus + About section content

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,44 @@ explorerToggler.addEventListener('click', function() {
 
     explorerFiles.classList.toggle('d-none');
 });
+
+const explorerMenuItems = document.querySelectorAll('.menu-item');
+const tabMenuItems = document.querySelectorAll('.tab-menu-item');
+
+explorerMenuItems.forEach((item) => {
+    item.addEventListener('click', event => {
+        showSection(event.target.dataset.section);
+        changeActiveTab(event.target, true);
+    });
+});
+
+tabMenuItems.forEach((item) => {
+    item.addEventListener('click', event => {
+        showSection(event.target.dataset.section);
+        changeActiveTab(event.target);
+    });
+});
+
+function showSection(sectionID) {
+    let sectionToShow = document.getElementById(sectionID);
+    let allSections = document.querySelectorAll('.portfolio-section');
+
+    allSections.forEach((section) => {
+        (section === sectionToShow ) ? 
+            section.classList.remove('d-none') : 
+            section.classList.add('d-none');
+    });
+}
+
+function changeActiveTab(clickedElement, isExplorerClick = false) {
+    let activeTab = document.querySelector('.tab-menu-item.tab.active');
+
+    if (isExplorerClick) {
+        activeTab.classList.remove('active');
+        document.querySelector(`.tab-menu-item.tab[data-section="${clickedElement.dataset.section}"`).classList.add('active');
+    } else {
+        activeTab.classList.remove('active');
+        clickedElement.classList.add('active');
+    }
+
+}
