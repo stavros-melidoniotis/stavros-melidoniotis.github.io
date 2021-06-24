19118cd2fc2b3143e6db6be408d430dd5809c3bb
Stavros Melidoniotis <melidon.stavros@gmail.com>
2021-06-24 19:44:58 +0300

    Final touches

---
+++ assets/js/hidden-message.js
@@ -0,0 +1,24 @@
+console.log(`
+    %c
+    Hey! I'm glad you made it here. This means you are a curious human being like me.
+
+    The previous version of this portfolio was based on a pre-made Bootstrap theme, so I
+    challenged myself to design and implement this one from scratch, in order to practice
+    my front-end skills. The theme is based on Visual Studio Code, the famous code editor 
+    from Microsoft. 
+    
+    More features coming soon!
+
+    If you like what you are seeing you can star the repository in my Github account.
+
+    Inspired by: 
+     - https://dev.to/itsnitinr/i-built-a-vscode-inspired-developer-portfolio-418g &
+     - https://dev.to/greedybrain/i-used-vscode-itself-as-my-portfolio-1fg5
+
+    `,
+    `
+    line-height: 1.5; 
+    font-size: .8rem;
+    font-family: JetBrains Mono, monospace;
+    `
+)
